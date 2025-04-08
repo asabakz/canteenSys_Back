01c71ac6398210e3022b20e1428a1ef8fc2f8732
asabakz <2421782070@qq.com>
2025-04-08 18:49:26 +0800

    update

--- Canteensys_web/src/main/java/com/item/web/category/entity/CategoryTable.java
+++ Canteensys_web/src/main/java/com/item/web/category/entity/CategoryTable.java
@@ -1,10 +1,14 @@
 package com.item.web.category.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.item.web.dish.entity.DishTable;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @TableName("category_table")
@@ -13,4 +17,6 @@ public class CategoryTable implements Serializable {
     private Long categoryId;
     private String categoryName;
     private Integer orderNum;
+    @TableField(exist = false)
+    private List<DishTable> dish = new ArrayList<DishTable>();
 }
---
+++ Canteensys_web/src/main/java/com/item/web/wechat_api/controller/HomePage.java
@@ -0,0 +1,65 @@
+package com.item.web.wechat_api.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.item.utils.ResultUtils;
+import com.item.utils.ResultVo;
+import com.item.web.ad.entity.AdTable;
+import com.item.web.ad.service.AdTableService;
+import com.item.web.dish.entity.DishTable;
+import com.item.web.dish.service.DishTableService;
+import com.item.web.dish_specs.entity.DishSpecsTable;
+import com.item.web.dish_specs.service.DishSpecsTableService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/homepage")
+public class HomePage {
+    @Autowired
+    private AdTableService adTableService;
+
+    @Autowired
+    private DishTableService dishTableService;
+
+    @Autowired
+    private DishSpecsTableService dishSpecsTableService;
+
+    // 首页轮播图
+    @GetMapping("/getSwiperList")
+    public ResultVo getSwiperList() {
+        QueryWrapper<AdTable> query = new QueryWrapper<>();
+        query.lambda().eq(AdTable::getOnload, "1");
+        List<AdTable> list = adTableService.list(query);
+        return ResultUtils.success("查询成功", list);
+    }
+
+    // 首页热销
+    @GetMapping("/getHotList")
+    public ResultVo getHotList() {
+        QueryWrapper<DishTable> query = new QueryWrapper<>();
+        query.lambda().eq(DishTable::getHot, "1")
+                .orderByAsc(DishTable::getOrderNum);
+        List<DishTable> list = dishTableService.list(query);
+
+        if (list.size() > 0) {
+            for (int i = 0; i < list.size(); i++) {
+                // 查询价格
+                QueryWrapper<DishSpecsTable> queryWrapper = new QueryWrapper<>();
+                queryWrapper.lambda().eq(DishSpecsTable::getDishId, list.get(i).getDishId())
+                        .orderByAsc(DishSpecsTable::getOrderNum);
+                List<DishSpecsTable> specs = dishSpecsTableService.list(queryWrapper);
+                list.get(i).setSpecs(specs);
+            }
+        }
+
+        return ResultUtils.success("查询成功", list);
+    }
+
+}
+
+
+
